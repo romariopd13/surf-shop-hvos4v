3395fdff26207ef7ffb4a3d34b01aea1fa0fbd99
Romario Pires <romario.pires@grupolife.med.br>
2021-08-12 20:38:26 -0300

    login front

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+  @ViewChild(IonSlides) slides: IonSlides;
+  wavesPosition: number = 0;
+  wavesDifference: number = 80;
   constructor() { }
 
   ngOnInit() {
   }
 
+  segmentChanged(event) {
+    if (event.detail.value === "login") {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference
+    } else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference
+    }
+  }
 }
